578803450131e734547dcd1601a5da758f68c5d4
JonathanC84 <jonathan.cayrol@gmail.com>
2024-09-06 15:09:59 +0200

    First commit : model, repo, controller and service created.

---
+++ src/main/java/com/halcyon/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.halcyon.employeemanager.repository;
+
+import com.halcyon.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findEmployeeById(Long id);
+}
---
+++ src/main/java/com/halcyon/employeemanager/service/EmployeeService.java
@@ -0,0 +1,47 @@
+package com.halcyon.employeemanager.service;
+
+import com.halcyon.employeemanager.exception.EmployeeNotFoundException;
+import com.halcyon.employeemanager.model.Employee;
+import com.halcyon.employeemanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class EmployeeService {
+
+    @Autowired
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepository.findEmployeeById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee " + id + "was not found"));
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+}
